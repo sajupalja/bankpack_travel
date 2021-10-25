a5acf50a5015245568e0eb07c3b0866b6acf52bf
cocony12 <apap8510@naver.com>
2021-10-25 15:19:22 +0900

    feat : recommend (survey rate data)

--- src/main/java/com/demo/microservices/controller/TravelRecommendController.java
+++ src/main/java/com/demo/microservices/controller/TravelRecommendController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.microservices.dao.TravelRecommendDao;
+import com.demo.microservices.model.TravelSurveyRateVO;
 import com.demo.microservices.model.TravelSurveyResultVO;
 import com.demo.microservices.model.TravelSurveyVO;
 
@@ -34,9 +35,17 @@ public class TravelRecommendController {
 	public ResponseEntity <List<TravelSurveyResultVO>> recommendWithSurvey(@RequestBody TravelSurveyVO survey){
 
 		List<TravelSurveyResultVO> list = null;
+		TravelSurveyRateVO rate = null;
 		String cluster = null;
 		
 		try {
+			
+			rate = travelRecommendDao.getSurveyRate(survey.getUserId(), survey.getTrvlMainFctr());
+			log.info("설문 데이터와 유저 기존 데이터를 기반으로 rate값 계산");
+			
+			survey.setTravelSurveyRateVO(rate); //rate 데이터 만든거 서베이에 넣어서 넘김
+			log.info("rate값 survey 데이터에 대입");
+			
 			log.info("여행지 설문을 기반으로 cluster 계산");
 			cluster = travelRecommendDao.getRecommendCluster(survey); //여행지 설문을 기반으로 cluster 계산
 			log.info("여행지 설문을 기반으로 cluster 계산한 값 :: "+cluster);
--- src/main/java/com/demo/microservices/model/TravelSurveyVO.java
+++ src/main/java/com/demo/microservices/model/TravelSurveyVO.java
@@ -18,9 +18,5 @@ public class TravelSurveyVO {
 	private String cmpnType; // 동행타입 
 	private String trvlMainFctr; // 여행중요도 
 	
-	private float totalRoomRate; // 총 숙소 비율 
-	private float totalFoodRate; // 총 식비 비율
-	private float totalTrffRate; // 총 교통 비율 
-	private float totalActRate; // 총 엑티비티 비율
-	private float totalEtcRate; // 총 기타 비율
+	private TravelSurveyRateVO travelSurveyRateVO; // 비용 비율
 }
