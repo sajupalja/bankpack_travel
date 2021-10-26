8f742bba1f79132a0e2d75fc276a6a2d35051b18
JANGDUCKIN <77999jno@naver.com>
2021-10-26 17:36:02 +0900

    fix : fix request dao and add img url column

--- src/main/java/com/demo/microservices/model/RequestTravelReviewVO.java
+++ src/main/java/com/demo/microservices/model/RequestTravelReviewVO.java
@@ -2,19 +2,17 @@ package com.demo.microservices.model;
 
 import java.util.Date;
 
+import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.Getter;
 
 @Getter
 @Setter
 @ToString
-public class TravelReviewVO {
+public class RequestTravelReviewVO {
 	private int trvlRevwId; // 후기내용 pk
 	private int trvlId; // 여행지 후기 pk
-	private Date writeDate; // 후기 작성 날짜 
-	private String revwText; // 후기 내용 
-	private int revwOrder; // 후기 작성 순서 
+	private String revwText; // 후기 내용
 	private Date trvlDt; // 여행일자
 	private String imgUrl; // 해당 후기의 사진
 	private int userId; // 작성자 pk
